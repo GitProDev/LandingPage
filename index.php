10fc293ddb30f5741890ba8435568ce7f9f423ab
Chiril <kirillserper@gmail.com>
2022-04-07 22:16:28 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body class="bg-dark text-white">
+    <header class="mb-auto">
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark border-bottom border-secondary p-3" style="margin-left: 2%; margin-right: 2%; ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="javascript:void(0)">WeExposeInfo</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="mynavbar">
+            <ul class="navbar-nav me-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">API</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">OSINT</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Database</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Redeem</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="javascript:void(0)">FAQ</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <!-- <input class="form-control me-2" type="text" placeholder="Search"> -->
+              <button class="btn btn-primary" type="button" style="margin-right: 10px;">Register</button>
+              <button class="btn btn-light" type="button">Login</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- <hr class="bg-danger border-2 border-top border-danger"> -->
+    <!-- <hr> -->
+    <main>
+      <div class="container-fluid mt-3">
+        <div class="text-center mt-5 pt-5">
+          <h1>Are you <span style="color: #38B2AC">secure</span>?</h1>
+          <h4>Search through our <span style="color: #38B2AC">9,972,791,410</span> records to find out!</h4>
+          <form class="w-25 me-auto mt-5" style="margin: auto;">
+            <input class="form-control me-2" type="text" placeholder="Search through our billions of records..." >
+            <div class="mt-3 d-flex justify-content-center">
+              <select class="form-select" id="country" required="" style="width: 220px; margin-right: 5px;">
+                <option value="">Email</option>
+                <option value="">Username</option>
+                <option value="">Email By Password</option>
+                <option value="">Username</option>
+              </select>
+              <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Search</b></button>
+            </div>
+          </form>
+        </div>
+        <br><br><br>
+        <h4 class="text-center" style="color: #CAD4DF">Functionality at It's best.</h4>
+        <div class="d-flex p-3 text-center justify-content-center" style="margin-left: 8%; margin-right: 8%;"><!-- bg-secondary -->
+          <div class="card" style="width:420px; height: 320px; margin: 1%; background-color: #2D3748">
+            <img class="" src="/landingpage/images/leaked.png" width="30%" alt="Card image" style="margin: auto; margin-top: 25px;">
+            <div class="card-body">
+              <h4 class="card-title">Fast & Efficient</h4>
+              <p class="card-text">Fetch up to 10,000 results per page in a fraction of a millisecond. Wildcard and Regex doesn't slow us down! View all our dehashed leakpeks today.</p>
+            </div>
+          </div>
+          <div class="card" style="width:420px; height: 320px; margin: 1%; background-color: #2D3748">
+            <img class="" src="/landingpage/images/database.png" width="30%" alt="Card image" style="margin: auto; margin-top: 25px;">
+            <div class="card-body">
+              <h4 class="card-title">Leaked Breached Dehashed</h4>
+              <p class="card-text">Gain access to an ever growing collection of over 10,000 data breached databases. We constantly acquire and import new leaked data every other week.</p>
+            </div>
+          </div>
+          <div class="card" style="width:420px; height: 320px; margin: 1%; background-color: #2D3748">
+            <img class="" src="/landingpage/images/breached.png" width="30%" alt="Card image" style="margin: auto; margin-top: 25px;">
+            <div class="card-body">
+              <h4 class="card-title">Powerful Search Operations</h4>
+              <p class="card-text">Fill in unknown parts of your query with Wildcard and Regex. Specify characters, length and more! Find all variations within seconds.</p>
+            </div>
+          </div>
+        </div>
+        <br><br><br><br><br><br><br><br>
+      <h1 class="text-center" style="color: #CAD4DF">Simple pricing.</h1>
+      <div class="text-center">
+        <img src="/landingpage/images/currencies.png" alt="Card image">  
+      </div>
+      <div class="d-flex p-3 text-center justify-content-center" style="margin-left: 8%; margin-right: 8%;">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width:490px; height: 320px; background-color: #2D3748">
+              <div class="card-body">
+                <h3 class="card-title">Trial</h3>
+                <h2>5$</h2>
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> <b>48 Hours Website Access</b></li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Unlimited Searches</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Download Databases</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> 24/7 Supportf</li>
+                  <li><img class="" src="/landingpage/images/close.png" alt="Card image" style="margin-bottom: 2px;"> API Access</li>
+                </ul>
+                <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Purchase</b></button>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width:490px; height: 320px; background-color: #2D3748">
+              <div class="card-body">
+                <h3 class="card-title">Simple</h3>
+                <h2>10$</h2>
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> <b>1 Week Website Access</b></li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Unlimited Searches</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Download Databases</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> 24/7 Supportf</li>
+                  <li><img class="" src="/landingpage/images/close.png" alt="Card image" style="margin-bottom: 2px;"> API Access</li>
+                </ul>
+                <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Purchase</b></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex p-3 text-center justify-content-center" style="margin-left: 8%; margin-right: 8%;">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width:320px; height: 320px; background-color: #2D3748">
+              <div class="card-body">
+                <h3 class="card-title">Professional</h3>
+                <h2>25$</h2>
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> <b>1 Month Website Access</b></li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Unlimited Searches</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Download Databases</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> 24/7 Supportf</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> API Access</li>
+                </ul>
+                <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Purchase</b></button>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width:320px; height: 320px;  background-color: #2D3748">
+              <div class="card-body">
+                <h3 class="card-title">Elite</h3>
+                <h2>70$</h2>
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> <b>3 Months Website Access</b></li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Unlimited Searches</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Download Databases</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> 24/7 Supportf</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> API Access</li>
+                </ul>
+                <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Purchase</b></button>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width:320px; height: 320px;  background-color: #2D3748">
+              <div class="card-body">
+                <h3 class="card-title">Enterprise</h3>
+                <h2>125$</h2>
+                <ul class="list-unstyled mt-3 mb-4">
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> <b>Lifetime Website Access</b></li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Unlimited Searches</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> Download Databases</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> 24/7 Supportf</li>
+                  <li><img class="" src="/landingpage/images/check.png" alt="Card image" style="margin-bottom: 2px;"> API Access</li>
+                </ul>
+                <button type="button" class="btn" style="background-color: #4299E1; color: white;"><b>Purchase</b></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <br><br><br><br><br><br>
+      <h1 class="text-center">Need help? Contact us here!</h1>
+      <div class="text-center">
+        <img src="/landingpage/images/telegram_icon.png" alt="Card image" style="margin: 15px; width: 55px;">  
+        <img src="/landingpage/images/leakpeek.png" alt="Card image" style="margin: 15px; width: 55px;"> 
+      </div>
+      </div>
+    </main>
+    <br>
+    <footer>
+      <p class="text-center" style="color: #CAD4DF">
+        ©2022 WeExposeInfo. All rights reserved.</br>
+        Terms of Service · Privacy Policy
+      </p>
+    </footer>
+</body>
+</html>
+
+
